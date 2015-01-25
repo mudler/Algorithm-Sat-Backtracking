bc32d4c540b1667df1c3fe909d50eb0423c82761
mudler <mudler@dark-lab.net>
2015-01-25 17:27:17 +0100

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e329321..3dcc73a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Algorithm-SAT-Backtracking
    
     {{$NEXT}}
    
    +0.08 2015-01-25T16:27:13Z
    +
         - POD fixes
    
     0.07 2015-01-25T16:11:36Z

--- lib/Algorithm/SAT/Expression.pm
+++ lib/Algorithm/SAT/Expression.pm
@@ -5,7 +5,7 @@ use warnings;
 require Algorithm::SAT::Backtracking;
 use Storable qw(dclone);
 use Carp qw(croak);
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 # Boolean expression builder.  Note that the connector for clauses is `OR`;
 # so, when calling the instance methods `xor`, `and`, and `or`, the clauses
